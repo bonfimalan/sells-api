fe2385647e821387395fd79be7fc3685af7890e4
bonfimalan <alanbonfim2000@gmail.com>
2023-01-12 20:46:48 -0300

    OrderProductService created

---
+++ src/main/java/io/github/bonfimalan/sells/service/OrderProductService.java
@@ -0,0 +1,11 @@
+package io.github.bonfimalan.sells.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class OrderProductService {
+    
+}
