0ebb7ea3021d18466adf51813ddd95adeca7b184
bonfimalan <alanbonfim2000@gmail.com>
2023-01-12 19:22:38 -0300

    Product service created and controller refactored, all communication logic with database in the service now

--- src/main/java/io/github/bonfimalan/sells/controller/ProductController.java
+++ src/main/java/io/github/bonfimalan/sells/controller/ProductController.java
@@ -2,9 +2,6 @@ package io.github.bonfimalan.sells.controller;
 
 import java.util.List;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,69 +12,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import io.github.bonfimalan.sells.domain.Product;
-import io.github.bonfimalan.sells.repository.ProductRepository;
+import io.github.bonfimalan.sells.service.ProductService;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
-    private ProductRepository repository;
+    private ProductService service;
 
     @GetMapping("/{id}")
-    public Product getByid(@PathVariable Integer id) {
-        return repository
-                .findById(id)
-                .orElseThrow(() -> notFoundException(id));
+    public Product getById(@PathVariable Integer id) {
+        return service.getById(id);
     }
 
     @GetMapping
     public List<Product> get(Product product) {
-        ExampleMatcher matcher = ExampleMatcher
-                .matching()
-                .withIgnoreCase()
-                .withStringMatcher(StringMatcher.CONTAINING);
-        Example<Product> example = Example.of(product, matcher);
-
-        return repository.findAll(example);
+        return service.findAllByFilter(product);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product post(@RequestBody Product product) {
-        return repository.save(product);
+        return service.save(product);
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void put(@PathVariable Integer id, @RequestBody Product product) {
-        repository
-                .findById(id)
-                .map(result -> {
-                    product.setId(result.getId());
-                    return repository.save(product);
-                })
-                .orElseThrow(() -> notFoundException(id));
+    public Product put(@PathVariable Integer id, @RequestBody Product product) {
+        return service.update(id, product);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
-        repository
-                .findById(id)
-                .map(result -> {
-                    repository.delete(result);
-                    return result;
-                })
-                .orElseThrow(() -> notFoundException(id));
-    }
-
-    private ResponseStatusException notFoundException(Integer id) {
-        return new ResponseStatusException(
-                HttpStatus.NOT_FOUND,
-                String.format("Product with id %id doesn't exist", id));
+        service.deleteById(id);
     }
 }
