f4e247705e7d32d94c4dfac003ace902fbd88c9d
bonfimalan <alanbonfim2000@gmail.com>
2023-01-12 19:07:24 -0300

    Handle to NotFound exception added in controller advice

--- src/main/java/io/github/bonfimalan/sells/controller/ApplicationRestControllerAdvice.java
+++ src/main/java/io/github/bonfimalan/sells/controller/ApplicationRestControllerAdvice.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.github.bonfimalan.sells.exception.ApiErrors;
+import io.github.bonfimalan.sells.exception.NotFoundException;
 import io.github.bonfimalan.sells.exception.ServiceException;
 
 @RestControllerAdvice
 public class ApplicationRestControllerAdvice {
     @ExceptionHandler(ServiceException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiErrors handleServiceException(ServiceException exception) {
+    public ApiErrors handleBadRequest(ServiceException exception) {
+        String message = exception.getMessage();
+        return new ApiErrors(message);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrors handleNotFound(NotFoundException exception) {
         String message = exception.getMessage();
         return new ApiErrors(message);
     }
